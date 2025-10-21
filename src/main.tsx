840662708b2b310a08ac2d349900312379e8cb50
Spencer Chang <spencerc99@gmail.com>
2025-10-20 22:09:04 -0700

    add icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import Page3 from "./pages/Page3.tsx";
 import Page4 from "./pages/Page4.tsx";
@@ -8,12 +9,14 @@ import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/3" element={<Page3 />} />
-        <Route path="/4" element={<Page4 />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/3" element={<Page3 />} />
+          <Route path="/4" element={<Page4 />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
--- src/pages/Page3.tsx
+++ src/pages/Page3.tsx
@@ -1,9 +1,17 @@
+import { Helmet } from "react-helmet-async";
 import { CDPlayer } from "../components/CDPlayer";
 
 export default function Page3() {
   return (
-    <div className="h-[100dvh] bg-[#F5F5DC] flex items-center justify-center overflow-hidden fixed inset-0 p-4 sm:p-8">
-      <CDPlayer />
-    </div>
+    <>
+      <Helmet>
+        <title>d2d</title>
+        <link rel="icon" type="image/jpeg" href="/d2d-icon.jpg" />
+        <link rel="apple-touch-icon" href="/d2d-icon.jpg" />
+      </Helmet>
+      <div className="h-[100dvh] bg-[#F5F5DC] flex items-center justify-center overflow-hidden fixed inset-0 p-4 sm:p-8">
+        <CDPlayer />
+      </div>
+    </>
   );
 }
